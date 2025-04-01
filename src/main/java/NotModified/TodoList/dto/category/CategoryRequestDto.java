cac7acbdb1089ea0b633bd01af8df986149986ba
doyeon <doyun20445@gmail.com>
2025-04-02 02:45:44 +0900

    feat: category dto 파일 추가

---
+++ src/main/java/NotModified/TodoList/dto/category/CategoryRequestDto.java
@@ -0,0 +1,23 @@
+package NotModified.TodoList.dto.category;
+
+import NotModified.TodoList.domain.Category;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryRequestDto {
+    private String userId;
+    private String name;
+    private String categoryColor;
+
+    public Category toEntity() {
+        return Category.builder()
+                .userId(userId)
+                .name(name)
+                .categoryColor(categoryColor)
+                .build();
+    }
+}
---
+++ src/main/java/NotModified/TodoList/dto/category/CategoryResponseDto.java
@@ -0,0 +1,21 @@
+package NotModified.TodoList.dto.category;
+
+import NotModified.TodoList.domain.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponseDto {
+    private String userId;
+    private String name;
+    private String categoryColor;
+
+    public CategoryResponseDto(Category category) {
+        this.userId = category.getUserId();
+        this.name = category.getName();
+        this.categoryColor = category.getCategoryColor();
+    }
+}
