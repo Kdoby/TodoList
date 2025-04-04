7a16f37c0fcf1cc812ea4416027c2ad4879cea04
doyeon <doyun20445@gmail.com>
2025-04-04 15:22:53 +0900

    feat: Add findByDate

--- src/main/java/NotModified/TodoList/repository/TodoRepository.java
+++ src/main/java/NotModified/TodoList/repository/TodoRepository.java
@@ -2,9 +2,13 @@ package NotModified.TodoList.repository;
 
 import NotModified.TodoList.domain.Todo;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository {
     Todo save(Todo todo);
+    Optional<Todo> findById(Long id);
+    List<Todo> findByDate(LocalDate date, String userId);
     List<Todo> findAll();
 }
