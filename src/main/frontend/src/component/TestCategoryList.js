eb601ec922833c866e500b73254a5cb318fe34c1
eheka78 <gemddkim22@gmail.com>
2025-04-10 05:06:13 +0900

    feat: category 삭제 코드 추가

--- src/main/frontend/src/component/TestCategoryList.js
+++ src/main/frontend/src/component/TestCategoryList.js
@@ -1,39 +1,47 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 import './TestCategoryList.css';
 
-const TestCategoryList = ({ categories }) => {
-    const deleteCategory = async() => {
-        alert("123");
-        // 카테고리 삭제 axios *********************************
+
+const TestCategoryList = ({ categories, fetchCategories, categoryMode }) => {
+
+    const deleteCategory = async (id) => {
+        try {
+            const response = await axios.delete('/api/categories/' + id);
+
+            if (response.data.success) {
+                alert(response.data.message);
+                fetchCategories(categoryMode);
+            } else {
+                alert("response error");
+            }
+        } catch (error) {
+            alert(error);
+        }
     };
 
     return (
         <div>
             <div id="categoryList">
                 {categories.map((category) => (
-                    <div style={{ margin: '5px 0px' }}>
+                    <div key={category.id} style={{ margin: '5px 0px' }}>
                         <span className="categoryColor"
-                              style={{backgroundColor: category.color}}
-                        ></span>
+                              style={{backgroundColor: category.color}}>
+                        </span>
 
-                        <span key={category.id} className="category">
+                        <span className="category">
                             {category.name || 'No Name'}
                         </span>
 
-                        <div className="setting-wrapper"
-                             style={{
-
-                        }}>
+                        <div className="setting-wrapper">
                             <span>setting</span>
-                            <ul class="setting">
+                            <ul className="setting">
                                 <li>edit</li>
                                 <li>inactive</li>
-                                <li onClick={deleteCategory}>delete</li>
+                                <li onClick={() => deleteCategory(category.id)}>delete</li>
                             </ul>
                         </div>
-
                     </div>
                 ))}
             </div>
