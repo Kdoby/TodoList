30961fa995bb4897aff9c22763eb743fbbaea273
doyeon <doyun20445@gmail.com>
2025-07-29 17:23:11 +0900

    feat: studyLog에 필드 추가

--- src/main/java/NotModified/TodoList/dto/studyLog/StudyLogResponse.java
+++ src/main/java/NotModified/TodoList/dto/studyLog/StudyLogResponse.java
@@ -1,12 +1,12 @@
 package NotModified.TodoList.dto.studyLog;
 
+import NotModified.TodoList.domain.StudyLog;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -14,9 +14,25 @@ import java.time.LocalTime;
 @AllArgsConstructor
 public class StudyLogResponse {
     private Long id;
-    private Long todoId;
-    private LocalDate startDate;
-    private LocalTime startTime;
-    private LocalDate endDate;
-    private LocalTime endTime;
+    private String todoTitle;
+    private String categoryName;
+    private String categoryColor;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Integer duration;
+    private Boolean isManual;
+
+    public static StudyLogResponse from(StudyLog s) {
+        return new StudyLogResponse(
+                s.getId(),
+                s.getTodoTitle(),
+                s.getCategoryName(),
+                s.getCategoryColor(),
+                s.getStartTime(),
+                s.getEndTime(),
+                s.getDuration(),
+                s.getIsManual()
+        );
+    }
 }
+
