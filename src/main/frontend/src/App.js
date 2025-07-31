a41fd2869a5c54136286c9f46970efe74bb92014
yuminmi <minming0400@naver.com>
2025-07-31 21:47:36 +0900

    feat: app.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,27 +2,15 @@ import TodoListPage from './component/TodoListPage';
 
 import React, {useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import StudyLogPage from "./component/StudyLogPage";
 
 
 
 function App() {
-    const [hello, setHello] = useState('');
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-
 
     return (
     <div>
-        <div>
-            TEST || 백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-
-        <TodoListPage />
+        <StudyLogPage />
         <br /><br />
     </div>
     );
