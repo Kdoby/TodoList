a29d0b84f376897ba2daed80b9f1e3af53d5da2b
yuminmi <minming0400@naver.com>
2025-08-15 16:29:41 +0900

    feat: 타임어택 시작, 정지, 중지, 재시작 api 호출 / complete후 처리 버튼, 함수 구현 / continue 시간추가 구현

--- src/main/frontend/src/component/TimeAttackMain.js
+++ src/main/frontend/src/component/TimeAttackMain.js
@@ -4,38 +4,18 @@ import { Link } from 'react-router-dom';
 import "./TimeAttackMain.css";
 import axios from "axios";
 
-function StudyTimeInput({onStart, userName, todayDate}) {
-    const [allTodos, setAllTodos] = useState([]);
+function StudyTimeInput({allTodos, onStart, userName, todayDate}) {
     const [mode, setMode] = useState("normal");
-    const [todo, setTodo] = useState('');
+    const [todo, setTodo] = useState(null); // [id, title]
     const [hour, setHour] = useState(1); // 기본 1시간
     const [minute, setMinute] = useState(0);
     const todoRef = useRef();
 
-    const fetchTodoList = async () => {
-        try {
-            const response = await axios.post('/api/todos/list', {
-                userId: userName,
-                todoDate: todayDate
-            });
-            setAllTodos(response.data.data);
-            console.log("투두 리스트 받아오기: ", response.data);
-        } catch(e) {
-            console.error("fail fetch: ", e);
-        }
-    };
-    useEffect(() => {
-        if(!userName || !todayDate) {
-            return;
-        }
-        fetchTodoList();
-    }, [userName, todayDate]);
-
     const handleSubmit = () => {
         const totalMinutes = hour * 60 + minute;
-        if(!todo) {
+        if(todo === '') {
             alert("todo를 선택해주세요");
-            todoRef.current.focus();
+            todoRef.current?.focus();
             return;
         }
         if(totalMinutes <= 0) {
@@ -60,20 +40,34 @@ function StudyTimeInput({onStart, userName, todayDate}) {
                 padding: "30px"
             }}>
                 <div>todo: <span>
-                                <select name="todoSelect" ref={todoRef} value={todo} onChange={(e) => setTodo(e.target.value)} required>
-                                    <option value="todo">-- not select --</option>
+                                <select name="todoSelect" ref={todoRef} value={todo?.[0] ?? ""} onChange={(e) => {
+                                    const id = Number(e.target.value);
+                                    if (!id) {
+                                        setTodo(null);
+                                        return;
+                                    }
+                                    const selectedOpt = e.target.selectedOptions[0];
+                                    const title = selectedOpt?.dataset?.title ?? "";
+                                    setTodo([id, title]);
+                                }} required>
+                                    <option value="">-- not select --</option>
                                     {Array.isArray(allTodos) &&
-                                        allTodos.flatMap((category, catIdx) =>
-                                            category.todos.map((todo, todoIdx) => (
-                                                <option key={todo.id} value={todo.id}>{category.categoryName} - {todo.title}</option>
+                                        allTodos.flatMap((category) =>
+                                            category.todos.map((t) => (
+                                                <option key={t.id} value={t.id} data-title={t.title}>{category.categoryName} - {t.title}</option>
                                             ))
                                         )}
                                 </select>
                             </span>
                 </div>
                 <div>time: <span>
-                    <input type="number" min="0" max="24" step="1" value={hour} onChange={(e) => setHour(Number(e.target.value))} />
-                    <input type="number" min="0" max="59" step="1" value={minute} onChange={(e) => setMinute(Number(e.target.value))} />
+                    <input type="number" min="0" max="24" step="1" value={hour} onChange={(e) => setHour(Math.max(0, Math.min(24, Number(e.target.value))))} />
+                    <input type="number" min="0" max="59" step="1" value={minute} onChange={(e) => {
+                        let v = Math.max(0, Math.min(59, Number(e.target.value)));
+                        // 24시간이면 분은 0으로 강제
+                        if (hour === 24 && v > 0) v = 0;
+                        setMinute(v);
+                    }} />
                 </span></div>
             </div>
 
@@ -92,11 +86,115 @@ function StudyTimeInput({onStart, userName, todayDate}) {
     );
 }
 
-function StudyTimer ({targetTime, mode, todo, onReset}) {
+function AddTime ({isOpen, isFinished, onInput, onAdd, closeModal}) {
+    const [hour, setHour] = useState(1); // 기본 1시간
+    const [minute, setMinute] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const totalMinutes = hour * 60 + minute;
+        if(totalMinutes <= 0) {
+            alert("0분 이상으로 설정해주세요.");
+            return;
+        }
+        if(isFinished) { // complete상태에서 시간 추가하는경우
+            onInput(totalMinutes);
+        } else {
+            onAdd(totalMinutes); // 타이머 진행중에 시간 추가하는 경우
+        }
+        closeModal();
+    };
+
+    return (
+        <div style={{display:isOpen?"block": "none",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "rgba(0,0,0,0.35)"}}>
+            <div style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "677px",
+                height: "498px",
+                backgroundColor: "white",
+                borderRadius: "20px",
+                border: "solid 1px"
+            }}>
+                <div style={{padding: "30px"}}>
+                    <img style={{ border: "none",
+                        cursor: "pointer",
+                        width:"40px",
+                        marginLeft: "580px"
+                    }} src={"./close.png"} alt={"closeModal"} onClick={closeModal}></img>
+
+                    <form className={"AT_form"} onSubmit={handleSubmit}>
+                        <div>time: <span>
+                            <input type="number" min="0" max="24" step="1" value={hour} onChange={(e) => setHour(Math.max(0, Math.min(24, Number(e.target.value))))} />
+                            <input type="number" min="0" max="59" step="1" value={minute} onChange={(e) => {
+                                let v = Math.max(0, Math.min(59, Number(e.target.value)));
+                                // 24시간이면 분은 0으로 강제
+                                if (hour === 24 && v > 0) v = 0;
+                                setMinute(v);
+                            }} />
+                        </span></div>
+                        <button className={"AT_submit"} type="submit" >추가</button>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+function CheckTodo({isOpen, closeModal, selectedTodo, onCheck}) {
+    return(
+        <div style={{display:isOpen?"block": "none",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "rgba(0,0,0,0.35)"}}>
+            <div style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "677px",
+                height: "498px",
+                backgroundColor: "white",
+                borderRadius: "20px",
+                border: "solid 1px"
+            }}>
+                <div style={{padding: "30px"}}>
+                    <h2 id="checkTodoTitle" style={{ fontSize: 18, fontWeight: 700, marginBottom: 12 }}>
+                        타이머 완료
+                    </h2>
+                    <p style={{ marginBottom: 24 }}>
+                        <strong>{selectedTodo?.title ?? "선택한 투두"}</strong>
+                        을(를) 완료로 체크할까요?
+                    </p>
+                    <button className={"AT_button"} type="button" onClick={onCheck}>체크</button>
+                    <button className={"AT_button"} type="button" onClick={closeModal}>닫기</button>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+function StudyTimer ({targetTime, mode, onStop, onPause, onResume, onContinue, onDone, getCurrentLocalDateTime}) {
     const [remainingSeconds, setRemainingSeconds] = useState(targetTime * 60);
     const [isPaused, setIsPaused] = useState(false);
     const [isFinished, setIsFinished] = useState(false);
 
+    const [CompletedTime, setCompletedTime] = useState(null);
     useEffect(() => {
         if(isPaused || isFinished) return;
 
@@ -104,6 +202,7 @@ function StudyTimer ({targetTime, mode, todo, onReset}) {
            setRemainingSeconds((prev) => {
                if (prev <= 1) {
                    clearInterval(timer);
+                   setCompletedTime(getCurrentLocalDateTime());
                    setIsFinished(true);
                    return 0;
                }
@@ -114,6 +213,17 @@ function StudyTimer ({targetTime, mode, todo, onReset}) {
         return () => clearInterval(timer);
     }, [isPaused, isFinished]);
 
+    // targetTime 변경시 감지 (실행중일때만)
+    const prevTargetTimeRef = useRef(targetTime);
+    useEffect(() => {
+        const prev = prevTargetTimeRef.current;
+        if(!isFinished && targetTime > prev) {
+            const addedSeconds = (targetTime - prev) * 60;
+            setRemainingSeconds((prevSec) => prevSec + addedSeconds);
+        }
+        prevTargetTimeRef.current = targetTime; // 업데이트
+    }, [targetTime, isFinished]);
+
     const hours = Math.floor(remainingSeconds / 3600);
     const minutes = Math.floor((remainingSeconds % 3600) / 60);
     const seconds = remainingSeconds % 60;
@@ -123,8 +233,8 @@ function StudyTimer ({targetTime, mode, todo, onReset}) {
             <div>
                 <h2>COMPLETE!</h2>
                 <div>
-                    <button>Continue</button>
-                    <button>Done</button>
+                    <button onClick={() => onContinue(isFinished)}>Continue</button>
+                    <button onClick={() => onDone(CompletedTime)}>Done</button>
                 </div>
             </div>
         );
@@ -135,14 +245,26 @@ function StudyTimer ({targetTime, mode, todo, onReset}) {
             <h2>{mode === "normal" ? "Normal Mode" : "🔥Hard Mode🔥"}</h2>
             <h2>{String(hours).padStart(2, "0")} : {String(minutes).padStart(2, "0")} : {String(seconds).padStart(2, "0")}</h2>
             {mode === "normal" ? (
-                <div>
-                    <button onClick={() => setIsPaused(!isPaused)}>
-                        {isPaused ? "RESTART" : "PAUSE"}
-                    </button>
-                    <button onClick={onReset} >
-                        STOP
-                    </button>
-                </div>
+                <>
+                    <p onClick={() => onContinue(isFinished)} style={{textDecoration: "underline", cursor:"pointer"}}>continue</p>
+                    <div>
+                        <button onClick={() => {
+                            if (!isPaused) {
+                                // 일시정지
+                                onPause?.();
+                            } else {
+                                // 재개
+                                onResume?.();
+                            }
+                            setIsPaused(!isPaused);
+                        }}>
+                            {isPaused ? "RESTART" : "PAUSE"}
+                        </button>
+                        <button onClick={onStop} >
+                            STOP
+                        </button>
+                    </div>
+                </>
             ) : (
                 <p style={{color: "gray", marginTop: "10px"}}>하드모드는 일시정지/종료할 수 없습니다.</p>
             )}
@@ -153,25 +275,204 @@ function StudyTimer ({targetTime, mode, todo, onReset}) {
 export default function TimeAttackMain({userName, todayDate}) {
     const [targetTime, setTargetTime] = useState(null); // 분 단위
     const [selectedMode, setSelectedMode] = useState("normal");
-    const [selectedTodo, setSelectedTodo] = useState('');
+    const [selectedTodo, setSelectedTodo] = useState(null); // [id, title]
+    const [startTime, setStartTime] = useState(null);
 
+    const [isAddOpen, setIsAddOpen] = useState(false);
+
+    // 처음 시작 / COMPLETE -> Continue에만 타이머 리마운트 하기위한 키
+    const [timerRunId, setTimerRunId] = useState(0);
+    const CompleteResetTimer = () => setTimerRunId(v => v + 1);
+
+    // 현재 LocalDateTime 받아오기
+    function getCurrentLocalDateTime() {
+        const d = new Date();
+        const pad = n => String(n).padStart(2, '0');
+        return `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())}`+
+            `T${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+    }
+
+    // 특정 일자 todo fetch
+    const [allTodos, setAllTodos] = useState([]);
+    const fetchTodos = async () => {
+        // console.log("userName: " + userName + ", todayDate: " + todayDate);
+
+        if(userName && todayDate) {
+            try {
+                const response = await axios.post('/api/todos/list', {
+                    userId: userName,
+                    todoDate: todayDate
+                });
+                setAllTodos(response.data.data);
+                console.log(allTodos);
+            } catch (e) {
+                console.error("fail fetch: ", e);
+            }
+        }
+
+    };
+
+    useEffect(() => {
+        if(!userName || !todayDate) {
+            return;
+        }
+        fetchTodos();
+    }, [userName, todayDate]);
+
+    // 처음 타이머 시작할때 입력받은 정보 저장, 첫 시작 시간 저장
+    // todo는 [id, title]
     const handleStart = (minutes, mode, todo) => {
         setSelectedTodo(todo);
         setSelectedMode(mode);
         setTargetTime(minutes);
+        setStartTime(getCurrentLocalDateTime());
+        CompleteResetTimer();
     }
 
-    const handleReset = () => {
-        // 공부시간 저장
-        setTargetTime(null);
+    // CheckTodo (중지 / complete 후 todo 체크리스트를 체크할 건지 선택받기
+    const [isCheckOpen, setIsCheckOpen] = useState(false);
+    const handleCheck = async () => {
+        if (!selectedTodo[0]) return;
+        console.log("check todo 클릭됨");
+        try {
+            const response = await axios.put('/api/todos/' + selectedTodo[0], {
+                title: selectedTodo[1],
+                isDone: true
+            });
+
+            if(response.data.success) {
+                alert(response.data.message);
+                setIsCheckOpen(false);
+                setSelectedTodo(null);
+                setSelectedMode("normal");
+                setTargetTime(null);
+            } else {
+                alert("todo check 실패");
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    // 중지시 첫 시작시간, 중지시간 API 저장 요청
+    // (중지 후 처리 필요)
+    const handleStop = async () => {
+        const endTime = getCurrentLocalDateTime();
+        if (!startTime) {
+            setSelectedTodo(null);
+            setSelectedMode("normal");
+            setTargetTime(null);
+            return;
+        }
+        try {
+            const res = await axios.post('/api/todo/log', {
+                userId: userName,
+                todoId: selectedTodo[0],
+                startTime: startTime,
+                endTime: endTime,
+                isManual: false
+            });
+            if (res.data.success) {
+                alert(res.data.message);
+                setIsCheckOpen(true);
+                // SelectedTodo, SelectedMode, TargetTime 초기화는 CheckTodo에서
+            }
+            else {
+                console.log("타이머 기록 등록 실패");
+            }
+        } catch (err) {
+            alert(`타이머 기록 등록 에러 발생: ${err?.response?.data?.message ?? err.message}`);
+        }
+    };
+
+    // 일시정지 후 재개 -> 시작시간 새로 저장
+    const handleResume = () => {
+        setStartTime(getCurrentLocalDateTime());
+    };
+
+    // 일시정지 -> 시작시간, 일시정지 시간 API 저장 요청
+    const handlePause = async () => {
+        const endTime = getCurrentLocalDateTime();
+        try {
+            const res = await axios.post('/api/todo/log', {
+                userId: userName,
+                todoId: selectedTodo[0],
+                startTime: startTime,
+                endTime: endTime,
+                isManual: false
+            });
+            if (res.data.success) {
+                alert(res.data.message);
+                setStartTime(null);
+            }
+            else {
+                console.log("타이머 기록 등록 실패");
+            }
+        } catch (err) {
+            alert(`타이머 기록 등록 에러 발생: ${err?.response?.data?.message ?? err.message}`);
+        }
+    };
+
+    const handleDone = async (endTime) => {
+        try {
+            const res = await axios.post('/api/todo/log', {
+                userId: userName,
+                todoId:selectedTodo[0],
+                startTime: startTime,
+                endTime: endTime,
+                isManual: false
+            });
+            if (res.data.success) {
+                alert(res.data.message);
+                setIsCheckOpen(true);
+                // SelectedTodo, SelectedMode, TargetTime 초기화는 CheckTodo에서
+            }
+            else {
+                console.log("타이머 기록 등록 실패");
+            }
+        } catch (err) {
+            alert(`타이머 기록 등록 에러 발생: ${err?.response?.data?.message ?? err.message}`);
+        }
+    }
+
+    // 시간 추가시 처리방법 구분용 (true -> addTimeAndRestart / false -> addTimeInProgress)
+    const [isFinished, setIsFinished] = useState(false);
+    // COMPLETE 화면에서 Continue 누르면 시간 추가 모달 열기
+    const openAddTime = (f) => {
+        setIsFinished(f);
+        setIsAddOpen(true);
+    };
+
+    // COMPLETE -> Continue -> 추가 시간 입력 처리 => 재시작
+    const addTimeAndRestart = (minutes) => {
+      setTargetTime(minutes); // 목표시간 새로 설정
+      setStartTime(getCurrentLocalDateTime()); // 시작 시각 갱신
+      CompleteResetTimer();
+      setIsAddOpen(false);
+    };
+
+    // timer 실행중에 추가 시간 입력 처리
+    const addTimeInProgress = (minutes) => {
+        setTargetTime(prev => (prev ?? 0) + minutes);
+        setIsAddOpen(false);
+        console.log("targetTime: ", targetTime);
     }
 
     return (
         <div>
             {targetTime === null ? (
-                <StudyTimeInput userName={userName} todayDate={todayDate} onStart={handleStart}/>
+                <StudyTimeInput allTodos={allTodos} userName={userName} todayDate={todayDate} onStart={handleStart} />
             ) : (
-                <StudyTimer targetTime={targetTime} mode={selectedMode} todo={selectedTodo} onReset={handleReset} />
+                <>
+                    <StudyTimer key={timerRunId} targetTime={targetTime} mode={selectedMode} todo={selectedTodo} onStop={handleStop} onPause={handlePause} onResume={handleResume} onContinue={openAddTime} onDone={handleDone} getCurrentLocalDateTime={getCurrentLocalDateTime} />
+                    <AddTime isOpen={isAddOpen} isFinished={isFinished} onInput={addTimeAndRestart} onAdd={addTimeInProgress} closeModal={()=>setIsAddOpen(false)} />
+                    <CheckTodo isOpen={isCheckOpen} onCheck={handleCheck} selectedTodo={selectedTodo} closeModal={() => {
+                        setIsCheckOpen(false);
+                        setSelectedTodo(null);
+                        setSelectedMode("normal");
+                        setTargetTime(null);
+                    }} />
+                </>
             )}
         </div>
     );
